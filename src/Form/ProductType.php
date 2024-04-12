4307bf98be93ca57f880807455e1924db75758ad
mjlinden <m.j.vd.linden@rocmondriaan.nl>
2024-04-12 14:44:42 +0200

    products crud

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -27,6 +29,14 @@ class ProductType extends AbstractType
             ])
             ->add('price',NumberType::class)
             ->add('stock')
+            ->add('category',EntityType::class,[
+                // looks for choices from this entity
+                'class' => Category::class,
+
+
+                'choice_label' => 'name',
+
+            ])
             ->add('submit',SubmitType::class)
         ;
     }
